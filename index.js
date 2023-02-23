89a5a5ace9e4ee63beccafdc6f51ffc4c878419e
Rebecca Louw <103456571+rebeccalouw@users.noreply.github.com>
2023-02-23 21:27:04 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-
-const APIkey= "f2c8f9ff"
+const APIkey= "8fe13269"
 
 const searchBar = document.getElementById("search-bar")
 const searchBtn = document.getElementById("search-btn")
 const moviesListDisplay = document.getElementById("movies-list-display")
 const initialIcon = document.getElementById("initial-icon")
 
-let moviesFoundArr = []
+let moviesFoundArray = []
 let movieSearch = []
+let movieHtml = ``
 let myWatchlist = JSON.parse(localStorage.getItem("watchlist")) || []
 
-
-searchBtn.addEventListener("click", getMovieList)
+searchBtn.addEventListener('click', getMoviesFromSearchBar)
 
 document.addEventListener("click", (e) => {
     if (e.target.matches(".addBtn")) {
@@ -20,14 +19,8 @@ document.addEventListener("click", (e) => {
   })
 
 
-function getMovieList(e) {
+function getMoviesFromSearchBar(e) {
     e.preventDefault()
-    getMoviesFromSearchBar() 
-}
-
-
-// uses the value of the search bar to send a request to the omdb API
-function getMoviesFromSearchBar() {
     moviesListDisplay.innerHTML = ""
     moviesListDisplay.classList.remove("initial-display")
     initialIcon.style.display = "none"
@@ -38,74 +31,75 @@ function getMoviesFromSearchBar() {
             if (data.Error) {
                 renderErrorMessage()
             } else {
-                moviesFoundArr = data.Search
-                for (let movie of moviesFoundArr) {
+                moviesFoundArray = data.Search
+                for (let movie of moviesFoundArray) {
                 getFullDetails(movie.imdbID)}
             }
         })
-}
-
+        searchBar.value = ""
+        }
 
 
 function getFullDetails(movieId) {
     fetch(`https://www.omdbapi.com/?apikey=${APIkey}&i=${movieId}&plot=full`)
                 .then(res => res.json())
-                .then(data => {
-                    let movieProperties = {
-                        id: data.imdbID,
-                    }
-                    movieSearch.push(movieProperties)
-                    renderHtml(data)
+                .then(data => { 
+                    movieSearch.push(data)
+                    const {Poster, Title, imdbRating, imdbID, Runtime, Genre, Plot} = data
+
+                    movieHtml += `
+                    <section class="movie-card">
+                        <img src="${Poster}">
+                        <div class="movie-content">
+                            <div class="movie-title">
+                                <h3>${Title}</h3>
+                                <p>⭐ ${imdbRating}</p>
+                            </div>
+                            <div class="movie-info">
+                                <p>${Runtime}</p>
+                                <p>${Genre}</p>
+                                <button class="addBtn" id="${imdbID}"><img class="plus-icon" src="images/plus-icon.svg" alt="+">Watchlist</button>
+                            </div>
+                            <div class="movie-body">
+                                <p>${Plot}</p>
+                            </div>
+                        </div>
+                    </section>
+                    `
+                    renderHtml(movieHtml)
                 })
+                moviesFoundArray = []
+                movieHtml = ``
 }
 
 
-function renderHtml(data) {
-
-    const {Poster, Title, imdbRating, imdbID, Runtime, Genre, Plot} = data
-
-    moviesListDisplay.innerHTML += `
-    <section class="movie-card">
-        <img src="${Poster}">
-        <div class="movie-content">
-            <div class="movie-title">
-                <h3>${Title}</h3>
-                <p>⭐ ${imdbRating}</p>
-            </div>
-            <div class="movie-info">
-                <p>${Runtime}</p>
-                <p>${Genre}</p>
-                <button class="addBtn" id="${imdbID}"><img class="plus-icon" src="images/plus-icon.svg" alt="+">Watchlist</button>
-            </div>
-            <div class="movie-body">
-                <p>${Plot}</p>
-            </div>
-        </div>
-    </section>
-    `
+function renderHtml(movieHtmlInfo) {
+    moviesListDisplay.innerHTML = movieHtmlInfo
 }
 
 
-  function addToMyWatchlist(el) {
+function addToMyWatchlist(el) {
     const id = el.target.id
-    const movie = movieSearch.find((movie) => movie.id === id)
-    const index = myWatchlist.findIndex((movie) => movie.id === id)
-    
+    const movie = movieSearch.find((movie) => movie.imdbID === id)
+    const index = myWatchlist.findIndex((movie) => movie.imdbID === id)
+
     // Adds the movie to the watchlist
     if (index === -1) {
         myWatchlist.push(movie)
         el.target.children[0].src = "images/check-icon.png"
         el.target.children[0].classList.remove("plus-icon")
         el.target.children[0].classList.add("check-icon")
-    } else {
+    } 
+    else {
     // Removes from the watchlist if it is already in it 
         myWatchlist.splice(index, 1)
         el.target.children[0].src = "images/plus-icon.svg"
         el.target.children[0].classList.remove("check-icon")
         el.target.children[0].classList.add("plus-icon")
     }
-  
+
     localStorage.setItem("watchlist", JSON.stringify(myWatchlist))
+
   }
 
 
@@ -115,7 +109,10 @@ function renderErrorMessage() {
 
 
 
+// research local storage
+// save to local storage
+// console.log error and call error function
 
-
-
-
+// build watchlist page
+// change background top
+// build mobile version
--- watchlist.js
+++ watchlist.js
@@ -1,17 +1,9 @@
-const APIkey= "f2c8f9ff"
+const APIkey= "8fe13269"
+
 
 const moviesWatchlistDisplay = document.getElementById("movies-watchlist-display")
 let myWatchlist = JSON.parse(localStorage.getItem("watchlist")) || []
-
-
-if(myWatchlist.length > 0) {
-    moviesWatchlistDisplay.innerHTML = ""
-    moviesWatchlistDisplay.classList.remove("initial-display")
-    for (let movie of myWatchlist) {
-        getFullDetails(movie.id)
-    }
-}
-
+let movieHtml = ``
 
 document.addEventListener("click", (e) => {
     if (e.target.matches(".removeBtn")) {
@@ -19,55 +11,63 @@ document.addEventListener("click", (e) => {
     }
   })
 
+  if(myWatchlist.length > 0) {
+    moviesWatchlistDisplay.innerHTML = ""
+    moviesWatchlistDisplay.classList.remove("initial-display")
+    for (let movie of myWatchlist) {
+        getFullDetails(movie.imdbID)
+    }
+}
+
 
 function getFullDetails(movieId) {
     fetch(`https://www.omdbapi.com/?apikey=${APIkey}&i=${movieId}&plot=full`)
                 .then(res => res.json())
-                .then(data => {
-                    renderHtml(data)
+                .then(data => { 
+                    const {Poster, Title, imdbRating, imdbID, Runtime, Genre, Plot} = data
+
+                    movieHtml += `
+                    <section class="movie-card">
+                        <img src="${Poster}">
+                        <div class="movie-content">
+                            <div class="movie-title">
+                                <h3>${Title}</h3>
+                                <p>⭐ ${imdbRating}</p>
+                            </div>
+                            <div class="movie-info">
+                                <p>${Runtime}</p>
+                                <p>${Genre}</p>
+                                <button class="removeBtn" id="${imdbID}"><img class="remove-icon" src="images/remove-icon.svg" alt="-">Watchlist</button>
+                            </div>
+                            <div class="movie-body">
+                                <p>${Plot}</p>
+                            </div>
+                        </div>
+                    </section>
+                    `
+                    renderHtml(movieHtml)
                 })
+                movieHtml = ``
 }
 
 
-function renderHtml(data) {
-
-    const {Poster, Title, imdbRating, imdbID, Runtime, Genre, Plot} = data
-
-    moviesWatchlistDisplay.innerHTML += `
-    <section class="movie-card">
-        <img src="${Poster}">
-        <div class="movie-content">
-            <div class="movie-title">
-                <h3>${Title}</h3>
-                <p>⭐ ${imdbRating}</p>
-            </div>
-            <div class="movie-info">
-                <p>${Runtime}</p>
-                <p>${Genre}</p>
-                <button class="removeBtn" id="${imdbID}"><img class="remove-icon" src="images/remove-icon.svg" alt="-">Watchlist</button>
-            </div>
-            <div class="movie-body">
-                <p>${Plot}</p>
-            </div>
-        </div>
-    </section>
-    `
+function renderHtml(movieHtmlInfo) {
+    moviesWatchlistDisplay.innerHTML = movieHtmlInfo
 }
-
 
 function removeFromMyWatchlist(el) {
-    myWatchlist = myWatchlist.filter((movie) => movie.id !== el.target.id)
+    myWatchlist = myWatchlist.filter((movie) => movie.imdbID !== el.target.id)
 
     if (myWatchlist.length > 0) {
         localStorage.setItem("watchlist", JSON.stringify(myWatchlist))
         for (let movie of myWatchlist) {
-            getFullDetails(movie.id)
+            getFullDetails(movie.imdbID)
             location.reload()
         }
     } else {
         localStorage.clear()
         moviesWatchlistDisplay.innerHTML = `
-        <h3>Your watchlist is looking a little empty...</h3>
+        <h3 class="empty-watchlist">Your watchlist is looking a little empty...</h3>
         <a href="index.html">
             <img class="plus-icon" src="images/plus-icon.svg" alt="+">
             Let's add some movies!
